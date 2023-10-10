49c5698fd2aec7c8940c1e66a6856735a42eaf54
Yáred Iessé Bustillo Medina <92419630+YaredBM@users.noreply.github.com>
2023-10-09 20:41:16 -0600

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,158 @@
+const wordList = [
+    {
+        word: "javascript",
+        hint: "Lenguaje de programación ampliamente utilizado en desarrollo web."
+    },
+    {
+        word: "html",
+        hint: "Lenguaje de marcado para la creación de páginas web."
+    },
+    {
+        word: "css",
+        hint: "Lenguaje de estilo para diseñar páginas web."
+    },
+    {
+        word: "programador",
+        hint: "Persona que escribe código de programación."
+    },
+    {
+        word: "desarrollo",
+        hint: "Proceso de crear aplicaciones y sitios web."
+    },
+    {
+        word: "tecnología",
+        hint: "El conjunto de herramientas y conocimientos para crear soluciones técnicas."
+    },
+    {
+        word: "basededatos",
+        hint: "Almacén de datos estructurados utilizado en aplicaciones web."
+    },
+    {
+        word: "framework",
+        hint: "Conjunto de herramientas y bibliotecas para facilitar el desarrollo de aplicaciones."
+    },
+    {
+        word: "servidor",
+        hint: "Computadora que proporciona servicios a otras computadoras en una red."
+    },
+    {
+        word: "backend",
+        hint: "La parte de un sitio web o aplicación que funciona en el servidor."
+    },
+    {
+        word: "frontend",
+        hint: "La parte visible de un sitio web o aplicación."
+    },
+    {
+        word: "interfaz",
+        hint: "La parte de una aplicación que interactúa con el usuario."
+    },
+    {
+        word: "navegador",
+        hint: "Software para acceder a páginas web en Internet."
+    },
+    {
+        word: "hacker",
+        hint: "Persona con habilidades técnicas que puede acceder a sistemas de manera no autorizada."
+    },
+    {
+        word: "criptografía",
+        hint: "Ciencia de proteger información mediante códigos y técnicas de encriptación."
+    },
+    {
+        word: "lenguaje",
+        hint: "Conjunto de instrucciones utilizado para escribir software."
+    },
+    {
+        word: "sistema",
+        hint: "Software que controla el funcionamiento de una computadora."
+    },
+    {
+        word: "cloudcomputing",
+        hint: "Tecnología que permite el acceso a recursos informáticos a través de Internet."
+    },
+    {
+        word: "codigo",
+        hint: "Instrucciones utilizadas para escribir software."
+    },
+    {
+        word: "realidad",
+        hint: "Entorno simulado generado por computadora."
+    },
+    {
+        word: "ciberataque",
+        hint: "Ataque realizado en línea a sistemas o datos."
+    },
+    {
+        word: "tecnologia",
+        hint: "Conjunto de herramientas y conocimientos técnicos."
+    },
+    {
+        word: "api",
+        hint: "Interfaz de programación de aplicaciones para conectar software."
+    },
+    {
+        word: "sitioweb",
+        hint: "Página o conjunto de páginas en línea con contenido y funcionalidad."
+    },
+    {
+        word: "navegador",
+        hint: "Software para acceder a páginas web en Internet."
+    },
+    {
+        word: "hacker",
+        hint: "Persona con habilidades técnicas que puede acceder a sistemas de manera no autorizada."
+    },
+    {
+        word: "redes",
+        hint: "Medios de comunicación en línea."
+    },
+    {
+        word: "internet",
+        hint: "Red global de computadoras interconectadas."
+    },
+    {
+        word: "pagina",
+        hint: "Hoja de un sitio web o libro."
+    },
+    {
+        word: "dispositivo",
+        hint: "Equipo electrónico o hardware."
+    },
+    {
+        word: "ciberseguridad",
+        hint: "Prácticas y medidas para proteger sistemas y datos en línea."
+    },
+    {
+        word: "ingenieria",
+        hint: "Proceso de diseño y desarrollo de software de alta calidad."
+    },
+    {
+        word: "tecnologico",
+        hint: "Relativo a la tecnología o el mundo digital."
+    },
+    {
+        word: "cifrado",
+        hint: "Proceso de codificación de datos para proteger la privacidad."
+    },
+    {
+        word: "hardware",
+        hint: "Componentes físicos de una computadora o dispositivo electrónico."
+    },
+    {
+        word: "softwarelibre",
+        hint: "Programas de código abierto disponibles para el público."
+    },
+    {
+        word: "sistemaplataforma",
+        hint: "Conjunto de software y hardware utilizado para realizar una tarea específica."
+    },
+    {
+        word: "desarrolloweb",
+        hint: "Proceso de creación de sitios y aplicaciones para Internet."
+    },
+    {
+        word: "tecnologia",
+        hint: "Conjunto de herramientas y conocimientos técnicos."
+    },
+];
