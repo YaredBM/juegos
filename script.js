04111955f4f31c3d32b973d119125644d75c2466
Yáred Iessé Bustillo Medina <92419630+YaredBM@users.noreply.github.com>
2023-10-09 20:36:51 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//Seleccionar todos los elementos
 const selectBox = document.querySelector(".select-box"),
 selectBtnX = selectBox.querySelector(".options .playerX"),
 selectBtnO = selectBox.querySelector(".options .playerO"),
@@ -9,109 +8,107 @@ resultBox = document.querySelector(".result-box"),
 wonText = resultBox.querySelector(".won-text"),
 replayBtn = resultBox.querySelector("button");
 
-window.onload = ()=>{ //Una vez se cargue la ventana
-    for (let i = 0; i < allBox.length; i++) { //añadir atributo onclick en todos los span disponibles
+window.onload = ()=>{
+    for (let i = 0; i < allBox.length; i++) {
        allBox[i].setAttribute("onclick", "clickedBox(this)");
     }
 }
 
 selectBtnX.onclick = ()=>{
-    selectBox.classList.add("hide"); //ocultar caja de selección
-    playBoard.classList.add("show"); //mostrar la sección playboard
+    selectBox.classList.add("hide");
+    playBoard.classList.add("show");
 }
 
 selectBtnO.onclick = ()=>{ 
-    selectBox.classList.add("hide"); //ocultar caja de selección
-    playBoard.classList.add("show"); //mostrar la sección playboard
-    players.setAttribute("class", "players active player"); //establecer atributo class en players con valores de players active player
+    selectBox.classList.add("hide");
+    playBoard.classList.add("show");
+    players.setAttribute("class", "players active player");
 }
 
-let playerXIcon = "fas fa-times"; //nombre de clase del icono de la cruz fontawesome
-let playerOIcon = "far fa-circle"; //nombre de clase del icono del círculo fontawesome
-let playerSign = "X"; //esta es una variable global porque la hemos usado en varias funciones
-let runBot = true; //esta tambien es una variable global con valor boolen...usamos esta variable para parar el bot una vez que el partido es ganado por alguien o empatado
+let playerXIcon = "fas fa-times",
+playerOIcon = "far fa-circle",
+playerSign = "X",
+runBot = true;
 
-// función clic del usuario
 function clickedBox(element){
     if(players.classList.contains("player")){
-        playerSign = "O"; //if player choose (O) then change playerSign to O
-        element.innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element/box
-        players.classList.remove("active"); ///add active class in players
-        element.setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
+        playerSign = "O";
+        element.innerHTML = `<i class="${playerOIcon}"></i>`;
+        players.classList.remove("active");
+        element.setAttribute("id", playerSign);
     }else{
-        element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element/box
-        element.setAttribute("id", playerSign); //establecer el atributo id en span/box con el signo elegido por el jugador
-        players.classList.add("active"); ///añadir clase activa en jugadores
+        element.innerHTML = `<i class="${playerXIcon}"></i>`;
+        element.setAttribute("id", playerSign);
+        players.classList.add("active");
     }
-    selectWinner(); //llamar a la función selectWinner
-    element.style.pointerEvents = "none"; //una vez que el usuario selecciona una casilla, no puede volver a hacer clic en ella
-    playBoard.style.pointerEvents = "none"; //añade pointerEvents none al playboard para que el usuario no pueda hacer click inmediatamente en ninguna otra casilla hasta que el bot seleccione
-    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed(); //generar un número aleatorio para que el bot se retrase aleatoriamente en seleccionar la casilla no seleccionada
+    selectWinner();
+    element.style.pointerEvents = "none";
+    playBoard.style.pointerEvents = "none";
+    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed();
     setTimeout(()=>{
-        bot(runBot); //llamando a la función bot
-    }, randomTimeDelay); //passing random delay value
+        bot(runBot);
+    }, randomTimeDelay);
 }
 
-// función de selección automática de bot
 function bot(){
-    let array = []; //creating empty array...we'll store unclicked boxes index
-    if(runBot){ //if runBot is true
-        playerSign = "O"; //change the playerSign to O so if player has chosen X then bot will O
+    let array = [];
+    if(runBot){
+        playerSign = "O";
         for (let i = 0; i < allBox.length; i++) {
-            if(allBox[i].childElementCount == 0){ //if the box/span has no children means <i> tag
-                array.push(i); //inserting unclicked boxes number/index inside array
+            if(allBox[i].childElementCount == 0){
+                array.push(i);
             }
         }
-        let randomBox = array[Math.floor(Math.random() * array.length)]; //getting random index from array so bot will select random unselected box
-        if(array.length > 0){ //if array length is greater than 0
+        let randomBox = array[Math.floor(Math.random() * array.length)];
+        if(array.length > 0){
             if(players.classList.contains("player")){ 
-                playerSign = "X"; //if player has chosen O then bot will X
-                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside bot selected element
-                allBox[randomBox].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-                players.classList.add("active"); ///add active class in players
+                playerSign = "X";
+                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
+                allBox[randomBox].setAttribute("id", playerSign);
+                players.classList.add("active");
             }else{
-                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside bot selected element
-                players.classList.remove("active"); //eliminar la clase active de los jugadores
-                allBox[randomBox].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
+                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
+                players.classList.remove("active");
+                allBox[randomBox].setAttribute("id", playerSign);
             }
-            selectWinner(); //llamar a la función selectWinner
+            selectWinner();
         }
-        allBox[randomBox].style.pointerEvents = "none"; //una vez que el bot selecciona una caja, el usuario no puede hacer click en esa caja
-        playBoard.style.pointerEvents = "auto"; //añade pointerEvents auto en el playboard para que el usuario pueda volver a hacer click en la casilla
-        playerSign = "X"; //si el jugador ha elegido X entonces el bot será O correcto entonces cambiamos el playerSign de nuevo a X para que el usuario sea X porque arriba hemos cambiado el playerSign a O para el bot
+        allBox[randomBox].style.pointerEvents = "none";
+        playBoard.style.pointerEvents = "auto";
+        playerSign = "X";
     }
 }
 
 function getIdVal(classname){
-    return document.querySelector(".box" + classname).id;//devuelve el valor de id
+    return document.querySelector(".box" + classname).id;
 }
-function checkIdSign(val1, val2, val3, sign){ //comprobar si el valor del id es igual al signo (X u O) o no si es si entonces devolver true
+function checkIdSign(val1, val2, val3, sign){ 
     if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
         return true;
     }
 }
-function selectWinner(){ //si la siguiente combinación ganadora coincide, selecciona el ganador
-    if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, jugadorSigno) || checkIdSigno(2,5,8, jugadorSigno) || checkIdSigno(3,6,9, jugadorSigno) || checkIdSigno(1,5,9, jugadorSigno) || checkIdSigno(3,5,7, jugadorSigno)){
-        runBot = false; //pasando el valor boolen false a runBot para que el bot no vuelva a ejecutarse
-        bot(runBot); //llamando a la función bot
-        setTimeout(()=>{ //después de que alguien gane la partida, oculta el tablero y muestra la caja de resultados después de 700ms
+function selectWinner(){
+    if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
+        runBot = false;
+        bot(runBot);
+        setTimeout(()=>{
             resultBox.classList.add("show");
             playBoard.classList.remove("show");
-        }, 700); //1s = 1000ms
-        wonText.innerHTML = `¡El jugador <p>${firmaDelJugador}</p> ha ganado la partida!`; //visualizar el texto ganador con la firma del jugador (X o O)
-    }else{ //si todas las casillas/elementos tienen valor id y todavía nadie gana entonces dibuja la partida
+        }, 700);
+        wonText.innerHTML = `¡Jugador <p>${playerSign}</p> ha ganado!`;
+    }else{
         if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
-            runBot = false; //pasando el valor boolen false a runBot para que el bot no vuelva a ejecutarse
-            bot(runBot); //llamar a la función bot
-            setTimeout(()=>{ //después de que el partido se dibuje, oculta el tablero y muestra la caja de resultados después de 700ms
+            runBot = false;
+            bot(runBot);
+            setTimeout(()=>{
                 resultBox.classList.add("show");
                 playBoard.classList.remove("show");
-            }, 700); //1s = 1000ms
-            wonText.textContent = "¡Se ha sorteado la partida!"; //visualizar el texto de sorteo de la partida
+            }, 700);
+            wonText.textContent = "¡Ha sido un empate!";
         }
     }
 }
 
 replayBtn.onclick = ()=>{
-    window.location.reload(); //recargar la página actual al pulsar el botón de reproducción
+    window.location.reload();
 }
